057d87676d8c8a0730808c0c34941c7596cea638
다은 <daeun9829@gmail.com>
2023-01-10 19:19:32 +0900

    2_week 업데이트, 4_dashboard 생성, base.css hover 수정

--- website/pages/2_week.php
+++ website/pages/2_week.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>index</title>
+    <title>Week</title>
     <link rel="stylesheet" href="../css/base.css">
     <link rel="stylesheet" href="../css/2_week.css">
 </head>
@@ -33,12 +33,12 @@
         <div class="lftSelect">
             <li class="lftSelectSection">고객 관리
                 <ul>
-                    <li class="now"><a href="">- DB</a></li>
+                    <li><a href="">- DB</a></li>
                 </ul>
             </li>
             <li class="lftSelectSection">배송 관리
                 <ul>
-                    <li><a href="">- WEEK</a></li>
+                    <li class="now"><a href="">- WEEK</a></li>
                     <li><a href="">- TODAY</a></li>
                 </ul>
             </li>
@@ -76,8 +76,9 @@
 
                     <thead>
                     <tr>
+                        <!--<th class=<?php /*echo $category_num=='1' ? "now" : ""; */?>>월요일</th>-->
                         <th>월요일</th>
-                        <th>화요일</th>
+                        <th class="today">화요일</th>
                         <th>수요일</th>
                         <th>목요일</th>
                         <th>금요일</th>
@@ -88,10 +89,194 @@
 
                     <tbody>
                     <tr>
-                        <td>총 주문: </td>
+                        <td class="tbSum"><!--월-->
+                            총 주문:
+                            <br>
+                            총 배달:
+                        </td>
+                        <td class="tbSum"><!--화-->
+                            총 주문:
+                            <br>
+                            총 배달:
+                        </td>
+                        <td class="tbSum"><!--수-->
+                            총 주문:
+                            <br>
+                            총 배달:
+                        </td>
+                        <td class="tbSum"><!--목-->
+                            총 주문:
+                            <br>
+                            총 배달:
+                        </td>
+                        <td class="tbSum"><!--금-->
+                            총 주문:
+                            <br>
+                            총 배달:
+                        </td>
+                        <td class="tbSum"><!--토-->
+                            총 주문:
+                            <br>
+                            총 배달:
+                        </td>
+                        <td class="tbSum"><!--일-->
+                            총 주문:
+                            <br>
+                            총 배달:
+                        </td>
                     </tr>
                     <tr>
-                        <td>총 배달: </td>
+                        <td class="tbTime"><!--월-->
+                            <b>아침</b>
+                            <br>
+                            [8:00]
+                            <br>
+                            [9:00]
+                        </td>
+                        <td class="tbTime"><!--화-->
+                            <b>아침</b>
+                            <br>
+                            [8:00]
+                            <br>
+                            [9:00]
+                        </td>
+                        <td class="tbTime"><!--수-->
+                            <b>아침</b>
+                            <br>
+                            [8:00]
+                            <br>
+                            [9:00]
+                        </td>
+                        <td class="tbTime"><!--목-->
+                            <b>아침</b>
+                            <br>
+                            [8:00]
+                            <br>
+                            [9:00]
+                        </td>
+                        <td class="tbTime"><!--금-->
+                            <b>아침</b>
+                            <br>
+                            [8:00]
+                            <br>
+                            [9:00]
+                        </td>
+                        <td class="tbTime"><!--토-->
+                            <b>아침</b>
+                            <br>
+                            [8:00]
+                            <br>
+                            [9:00]
+                        </td>
+                        <td class="tbTime"><!--일-->
+                            <b>아침</b>
+                            <br>
+                            [8:00]
+                            <br>
+                            [9:00]
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="tbTime"><!--월-->
+                            <b>점심</b>
+                            <br>
+                            [11:00]
+                            <br>
+                            [12:00]
+                        </td>
+                        <td class="tbTime"><!--화-->
+                            <b>점심</b>
+                            <br>
+                            [11:00]
+                            <br>
+                            [12:00]
+                        </td>
+                        <td class="tbTime"><!--수-->
+                            <b>점심</b>
+                            <br>
+                            [11:00]
+                            <br>
+                            [12:00]
+                        </td>
+                        <td class="tbTime"><!--목-->
+                            <b>점심</b>
+                            <br>
+                            [11:00]
+                            <br>
+                            [12:00]
+                        </td>
+                        <td class="tbTime"><!--금-->
+                            <b>점심</b>
+                            <br>
+                            [11:00]
+                            <br>
+                            [12:00]
+                        </td>
+                        <td class="tbTime"><!--토-->
+                            <b>점심</b>
+                            <br>
+                            [11:00]
+                            <br>
+                            [12:00]
+                        </td>
+                        <td class="tbTime"><!--일-->
+                            <b>점심</b>
+                            <br>
+                            [11:00]
+                            <br>
+                            [12:00]
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="tbTime last"><!--월-->
+                            <b>저녁</b>
+                            <br>
+                            [18:00]
+                            <br>
+                            [19:00]
+                        </td>
+                        <td class="tbTime last"><!--화-->
+                            <b>저녁</b>
+                            <br>
+                            [18:00]
+                            <br>
+                            [19:00]
+                        </td>
+                        <td class="tbTime last"><!--수-->
+                            <b>저녁</b>
+                            <br>
+                            [18:00]
+                            <br>
+                            [19:00]
+                        </td>
+                        <td class="tbTime last"><!--목-->
+                            <b>저녁</b>
+                            <br>
+                            [18:00]
+                            <br>
+                            [19:00]
+                        </td>
+                        <td class="tbTime last"><!--금-->
+                            <b>저녁</b>
+                            <br>
+                            [18:00]
+                            <br>
+                            [19:00]
+                        </td>
+                        <td class="tbTime last"><!--토-->
+                            <b>저녁</b>
+                            <br>
+                            [18:00]
+                            <br>
+                            [19:00]
+                        </td>
+                        <td class="tbTime last"><!--일-->
+                            <b>저녁</b>
+                            <br>
+                            [18:00]
+                            <br>
+                            [19:00]
+                        </td>
                     </tr>
                     </tbody>
 
